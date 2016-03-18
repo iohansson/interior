f26e9952ff11e4097a55ee38981c7fa69284f229
iohansson <w.iohansson@gmail.com>
2016-03-18 12:40:04 +0300

    cover for image

--- app/components/Image.jsx
+++ app/components/Image.jsx
@@ -1,18 +1,64 @@
 import React from 'react';
 
 export default class Image extends React.Component {
-  componentWillMount() {
-    const img = <img {...this.props} />;
-    img.onload = this.cover.bind(this);
-
-    this.img = img;
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: {
+        width: 0,
+        height: 0,
+        top: 0,
+        left: 0
+      }
+    };
   }
   render() {
     return (
-      this.img
+      <div className={this.props.className + '-container'}>
+        <img {...this.props}
+          onLoad={this.afterLoad.bind(this)}
+          style={this.state.style} />
+      </div>
     );
   }
-  cover() {
-    
+  getNaturalDimensions(el) {
+    return ({
+      ow: el.naturalWidth,
+      oh: el.naturalHeight,
+      ratio: el.naturalWidth / el.naturalHeight
+    });
+  }
+  getParentDimensions(el) {
+    const parent = el.parentNode;
+    return ({
+      pw: parent.clientWidth,
+      ph: parent.clientHeight,
+      pratio: parent.clientWidth / parent.clientHeight
+    });
+  }
+  cover(el) {
+    const { ow, oh, ratio } = this.getNaturalDimensions(el);
+    const { pw, ph, pratio} = this.getParentDimensions(el);
+    var width, height, top = 0, left = 0;
+    if (ratio > pratio) {
+      height = ph;
+      width = height * ratio;
+      left = (pw - width) / 2;
+    } else {
+      width = pw;
+      height = width / ratio;
+      top = (ph - height / 2);
+    }
+    this.setState({
+      style: {
+        width,
+        height,
+        top,
+        left
+      }
+    });
+  }
+  afterLoad(e) {
+    this.cover(e.target);
   }
 }
