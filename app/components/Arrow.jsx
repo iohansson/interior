410becf9d252f4cc9e9217bb82b691cfba7acc8a
iohansson <w.iohansson@gmail.com>
2016-03-28 15:23:13 +0300

    fixed arrows animation

--- app/components/Arrow.jsx
+++ app/components/Arrow.jsx
@@ -4,10 +4,30 @@ import { hashHistory } from 'react-router';
 export default class Arrow extends React.Component {
   constructor(props) {
     super(props);
+  }
+  setHoverTimeline() {
+    const {type} = this.props;
+    const down = type === 'down';
+    const movepx = down ? 16 : -16;
+    const h = 70,
+      largeh = h + Math.abs(movepx);
+    const yi = down ? 0 : 20.7;
+    const tl = new TimelineMax();
+    tl.to('#polygon'+type, 0.25, { y: movepx }, 0);
+    tl.to('#rect'+type, 0.25, { attr: {
+      height: largeh,
+      y: (down ? yi : yi + movepx)
+    } }, 0);
+    tl.to('#rect'+type, 0.25, { attr: {
+      height: h,
+      y: yi + movepx
+    } });
+    tl.pause();
 
-    this.state = {
-      hoverEnabled: false
-    }
+    this.timeline = tl;
+  }
+  componentDidMount() {
+    this.setHoverTimeline();
   }
   navigate() {
     hashHistory.push(this.props.link);
@@ -16,91 +36,10 @@ export default class Arrow extends React.Component {
     return false;
   }
   handleHover(hover) {
-    if (hover || this.state.hoverEnabled) {
-      const {type} = this.props;
-      const down = type === 'down';
-      const movepx = down ? 16 : -16;
-      const h = 70,
-        largeh = h + Math.abs(movepx);
-      const yi = down ? 0 : 20.7;
-      const tl = new TimelineMax();
-      const stages = {
-        hover: [
-          {
-            id: '#polygon'+type,
-            duration: 0.25,
-            to: {
-              y: movepx
-            },
-            start: 0
-          },
-          {
-            id: '#rect'+type,
-            duration: 0.25,
-            to: {
-              attr: {
-                height: largeh,
-                y: down ? yi : yi + movepx
-              }
-            },
-            start: 0
-          },
-          {
-            id: '#rect'+type,
-            duration: 0.25,
-            to: {
-              attr: {
-                height: h,
-                y: yi + movepx
-              }
-            }
-          }
-        ],
-        idle: [
-          {
-            id: '#rect'+type,
-            duration: 0.25,
-            to: {
-              attr: {
-                height: largeh,
-                y: down ? yi : yi + movepx
-              }
-            }
-          },
-          {
-            id: '#polygon'+type,
-            duration: 0.25,
-            to: {
-              y: 0
-            },
-            start: 0.25
-          },
-          {
-            id: '#rect'+type,
-            duration: 0.25,
-            to: {
-              attr: {
-                height: h,
-                y: yi
-              }
-            },
-            start: 0.25
-          }
-        ]
-      };
-      const animation = hover ? 'hover' : 'idle';
-      stages[animation].forEach((stage) => {
-        if (stage.start !== undefined) {
-          tl.to(stage.id, stage.duration, stage.to, stage.start);
-        } else {
-          tl.to(stage.id, stage.duration, stage.to);
-        }
-      });
-      tl.play();
-
-      this.setState({
-        hoverEnabled: true
-      });
+    if (hover) {
+      this.timeline.play();
+    } else {
+      this.timeline.reverse();
     }
   }
   render() {
