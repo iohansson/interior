73f376ef68a694a642b9d71485890b352043dd74
iohansson <w.iohansson@gmail.com>
2016-04-13 00:25:17 +0300

    gallery icon and gallery basic page

--- app/components/MenuItem.jsx
+++ app/components/MenuItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
+import GridIcon from './GridIcon.jsx';
 
 export default class MenuItem extends React.Component {
   render() {
     const { item, prefix, active } = this.props;
     const { name } = item;
+    const gridIcon = (name.toLowerCase() === 'projects' && active) ? <Link to={prefix+name+'/gallery'}><GridIcon /></Link> : null;
     return (
       <li className={'menu-list-item menu-item' + (active ? ' menu-item-active' : '') }>
         <Link className="menu-link menu-item-link" to={prefix+name}>{name}</Link>
+        { gridIcon }
       </li>
     );
   }
--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -4,6 +4,7 @@ import Index from '../components/Index.jsx';
 import Design from '../components/Design.jsx';
 import Projects from '../components/Projects.jsx';
 import ProjectListContainer from '../components/ProjectListContainer.jsx';
+import ProjectGalleryContainer from '../components/ProjectGalleryContainer.jsx';
 import About from '../components/About.jsx';
 import { Router, Route, hashHistory, IndexRoute, IndexRedirect } from 'react-router';
 
@@ -16,6 +17,7 @@ const routes = (
         <Route path="projects" component={Projects}>
           <Route path="list(/:projectId)" component={ProjectListContainer} />
           <IndexRedirect to="list" />
+          <Route path="gallery" component={ProjectGalleryContainer} />
         </Route>
         <Route path="about(/:pageId)" component={About} onEnter={About.resolve} />
       </Route>
